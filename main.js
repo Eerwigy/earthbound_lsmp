99673e46befda70a2a2d3dd3632bba666e6405e6
Eerwig <er3r3r@protonmail.com>
2026-01-13 20:45:22 -0500

    Code cleanup

--- main.js
+++ main.js
@@ -3,14 +3,17 @@
 
 const BAN_TIME = 1000 * 60 * 60;
 const MOB_SPAWN_CHANCE = 1 / 800;
+const MOB_SPAWN_DIST = 25;
+const MOB_HERD_SIZE = 4;
 const DAY_LENGTH = 1000 * 60 * 3;
+const LIFESTEAL = 10;
 
-var night = false;
+var isNight = false;
 
 tick = () => {
   const now = Date.now();
   const incl = now / DAY_LENGTH;
-  night = Math.floor(incl + 0.5) % 2 == 1;
+  isNight = Math.floor(incl + 0.5) % 2 == 1;
 
   for (const id of api.getPlayerIds()) {
     api.setClientOption(id, "skyBox", {
@@ -33,31 +36,35 @@ onPlayerJoin = (id) => {
   removeMsChest(id);
 
   const item = api.getMoonstoneChestItemSlot(id, 0);
-  if ((item?.attributes?.customDisplayName || null) != "Data3") {
+  let init = item?.attributes?.customDisplayName === "Data3";
+
+  if (!init) {
     setData(id, { health: 100, banTime: 0 });
-  } else {
-    const banTime = item.attributes.customAttributes.banTime;
-    if (banTime > Date.now()) {
-      api.broadcastMessage(
-        `${api.getEntityName(id)} is still too weak to continue ...`,
-        { color: "cyan" },
-      );
-      api.kickPlayer(id, "No more HP");
-      return;
-    }
-    const health = item.attributes.customAttributes.health;
-    api.setClientOption(id, "maxHealth", health);
+    return;
   }
+
+  const { banTime, health } = item.attributes.customAttributes;
+
+  if (banTime > Date.now()) {
+    api.broadcastMessage(
+      `${api.getEntityName(id)} is still too weak to continue ...`,
+      { color: "cyan" },
+    );
+    api.kickPlayer(id, "No more HP");
+    return;
+  }
+
+  api.setClientOption(id, "maxHealth", health);
 };
 
 onPlayerKilledOtherPlayer = (
   killer,
-  died,
+  victim,
 ) => {
   const killerName = api.getEntityName(killer);
-  const diedName = api.getEntityName(died);
+  const diedName = api.getEntityName(victim);
 
-  if (killer == died) {
+  if (killer == victim) {
     return;
   }
 
@@ -65,26 +72,26 @@ onPlayerKilledOtherPlayer = (
     color: "red",
   });
 
-  const oldHealthDied = api.getClientOption(died, "maxHealth");
-  if (oldHealthDied <= 10) {
+  const oldVictimHealth = api.getClientOption(victim, "maxHealth");
+  if (oldVictimHealth <= LIFESTEAL) {
     api.broadcastMessage(`${diedName} was too weak to continue ...`, {
       color: "cyan",
     });
 
-    api.kickPlayer(died, "No more HP");
-    setData(died, { health: 10, banTime: Date.now() + BAN_TIME });
+    api.kickPlayer(victim, "No more HP");
+    setData(victim, { health: LIFESTEAL, banTime: Date.now() + BAN_TIME });
     return;
   }
 
-  const oldHealthKiller = api.getClientOption(killer, "maxHealth");
-  const newKillerHealth = oldHealthKiller + 10;
-  const newDiedHealth = oldHealthDied - 10;
+  const oldKillerHealth = api.getClientOption(killer, "maxHealth");
+  const newKillerHealth = oldKillerHealth + LIFESTEAL;
+  const newVictimHealth = oldVictimHealth - LIFESTEAL;
 
   setData(killer, { health: newKillerHealth, banTime: 0 });
-  setData(died, { health: newDiedHealth, banTime: 0 });
+  setData(victim, { health: newVictimHealth, banTime: 0 });
 
   api.setClientOption(killer, "maxHealth", newKillerHealth);
-  api.setClientOption(died, "maxHealth", newDiedHealth);
+  api.setClientOption(victim, "maxHealth", newVictimHealth);
 };
 
 onInventoryUpdated = (id) => {
@@ -92,13 +99,13 @@ onInventoryUpdated = (id) => {
 };
 
 function spawnMobs(id) {
-  if (night) {
+  if (isNight) {
     if (Math.random() < MOB_SPAWN_CHANCE) {
       const [x, y, z] = api.getPosition(id);
-      const offsetX = x + Math.random() * 25;
-      const offsetZ = z + Math.random() * 25;
+      const offsetX = x + Math.random() * MOB_SPAWN_DIST;
+      const offsetZ = z + Math.random() * MOB_SPAWN_DIST;
       const herdId = api.createMobHerd();
-      const mobN = Math.ceil(Math.random() * 4);
+      const mobN = Math.ceil(Math.random() * MOB_HERD_SIZE);
       for (let i = 0; i < mobN; i += 1) {
         api.attemptSpawnMob("Draugr Zombie", offsetX, y, offsetZ, {
           mobHerdId: herdId,
